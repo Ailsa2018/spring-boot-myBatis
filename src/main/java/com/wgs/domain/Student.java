764061896051ccf3ad86b2ad6fec5dc84b9ece18
zheng <zhenghairong2017@126.com>
2018-04-18 09:26:30 +0800

    练习使用自动生成mybatis

---
+++ src/main/java/com/wgs/domain/Student.java
@@ -0,0 +1,33 @@
+package com.wgs.domain;
+
+public class Student {
+    private Long id;
+
+    private String name;
+
+    private String classname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname == null ? null : classname.trim();
+    }
+}
